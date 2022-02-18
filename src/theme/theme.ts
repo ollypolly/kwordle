48bcb967a7e8b4d35f6ccb908d15fc23a7de5c65
Olly Pethick <oliver.pethick@kwalee.com>
2022-02-18 22:56:10 +0000

    Add color mode support

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,47 @@
 import { createTheme } from "@mui/material/styles";
 
-export const theme = createTheme({
+const typography = {
+  h1: {
+    fontSize: "1rem",
+    fontWeight: 600,
+  },
+  h2: {
+    fontSize: "1.5rem",
+    fontWeight: 500,
+  },
+  subtitle1: {
+    fontSize: "0.9rem",
+    fontWeight: 400,
+  },
+  subtitle2: {
+    fontSize: "0.8rem",
+    fontWeight: 300,
+  },
+};
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    background: {
+      default: "#fff",
+      paper: "#fff",
+    },
+    text: {
+      primary: "rgba(255,255,255,0.87)",
+      secondary: "rgba(220,220,220,0.54)",
+      disabled: "rgba(133,133,133,0.38)",
+    },
+  },
+  ...typography,
+});
+
+export const darkTheme = createTheme({
   palette: {
     mode: "dark",
     primary: {
@@ -19,22 +60,5 @@ export const theme = createTheme({
       disabled: "rgba(133,133,133,0.38)",
     },
   },
-  typography: {
-    h1: {
-      fontSize: "1rem",
-      fontWeight: 600,
-    },
-    h2: {
-      fontSize: "1.5rem",
-      fontWeight: 500,
-    },
-    subtitle1: {
-      fontSize: "0.9rem",
-      fontWeight: 400,
-    },
-    subtitle2: {
-      fontSize: "0.8rem",
-      fontWeight: 300,
-    },
-  },
+  ...typography,
 });
