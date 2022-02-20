045d97c8a2a0ea2769326d361b772957acab5722
Olly Pethick <oliver.pethick@kwalee.com>
2022-02-20 20:40:00 +0000

    Calculate differences between guess and target

--- src/components/GuessRow.tsx
+++ src/components/GuessRow.tsx
@@ -1,26 +1,82 @@
-import { Box } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import React from "react";
+import gameData from "../data/kwalee-data";
 import { GuessMetrics } from "../model/games";
 import { Guess } from "../model/guess";
 import { Letter } from "./Logo/Letter";
 
 export type GuessRowProps = {
+  index: number;
   guess?: Guess;
 };
 
-export function GuessRow({ guess }: GuessRowProps) {
+export function GuessRow({ guess, index }: GuessRowProps) {
+  const theme = useTheme();
+
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      {Object.entries(GuessMetrics).map(([key, value], index) => (
-        <Letter
-          tooltipTitle={value}
-          key={index}
-          backgroundColor={guess && "#424242"}
-          borderColor={guess ? "#424242" : "#a5a5a5"}
+    <>
+      {index === 0 && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          {}
-        </Letter>
-      ))}
-    </Box>
+          <Typography
+            variant="body1"
+            sx={{ width: "140px", textAlign: "center" }}
+          >
+            Name
+          </Typography>
+
+          {Object.entries(GuessMetrics).map(([key, value], index) => (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+                flexGrow: 0,
+                margin: theme.spacing(0.5),
+                width: "50px",
+                height: "50px",
+                padding: theme.spacing(0.5),
+                [theme.breakpoints.down("sm")]: {
+                  width: "40px",
+                  height: "40px",
+                },
+              }}
+            >
+              <Typography>{value}</Typography>
+            </Box>
+          ))}
+        </Box>
+      )}
+      <Box
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Typography
+          variant="body1"
+          sx={{ width: "140px", textAlign: "center" }}
+        >
+          {guess?.name}
+        </Typography>
+
+        {Object.entries(GuessMetrics).map(([key, value], index) => (
+          <Letter
+            //@ts-ignore
+            tooltipTitle={
+              //@ts-ignore
+              guess && gameData[guess.name] && gameData[guess.name][key]
+            }
+            key={index}
+            backgroundColor={guess && "#424242"}
+            borderColor={guess ? "#424242" : "#a5a5a5"}
+          >
+            {guess && guess[key as keyof Guess].toString()}
+          </Letter>
+        ))}
+      </Box>
+    </>
   );
 }
