a32673e9952a6a9353dc266240a2deb0895b4d78
Olly Pethick <oliver.pethick@kwalee.com>
2022-02-20 01:24:41 +0000

    Add game dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
-import { Box, Button, Typography, useTheme } from "@mui/material";
+import { Autocomplete, Box, Button, TextField, useTheme } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { selectColorMode, setColorMode } from "./features/general/generalSlice";
 import { ColorMode } from "./model/colorMode";
 import { DarkModeSwitch } from "./components/DarkModeSwitch";
 import { Logo } from "./components/Logo/Logo";
+import gameData from "./data/kwalee-data.json";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -42,6 +43,13 @@ function App() {
           margin: theme.spacing(1),
         }}
       >
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={Object.keys(gameData)}
+          sx={{ width: 300, marginRight: theme.spacing(1) }}
+          renderInput={(params) => <TextField {...params} label="Game" />}
+        />
         <Button variant="contained">Submit</Button>
       </Box>
     </Box>
