ccfa955ca817f23452eefb57bb04c4d752642144
Olly Pethick <oliver.pethick@kwalee.com>
2022-02-21 01:01:44 +0000

    Fix flippable props error

--- src/components/Letter/Flippable.tsx
+++ src/components/Letter/Flippable.tsx
@@ -5,9 +5,9 @@ import "./letter-flip.css";
 
 type FlippableProps = {
   startFlip?: boolean;
-};
+} & PropsWithChildren<any>;
 
-export function Flippable({ children, startFlip }: PropsWithChildren<any>) {
+export function Flippable({ children, startFlip }: FlippableProps) {
   return (
     <Box className={clsx("flip-container", startFlip && "flip-this")}>
       <Box className="flipper">{children}</Box>
